225aae5da7f2b89cd72163551f8d2d4ea6e4d6e0
rniemela77 <rvniemela@hotmail.com>
2025-06-12 12:42:48 -0700

    install phaserjs and browsersync

---
+++ bs-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    server: {
+        baseDir: "./",
+        index: "index.html"
+    },
+    files: [
+        "./**/*.{html,js,css}"
+    ],
+    open: false,
+    notify: false
+}; 
